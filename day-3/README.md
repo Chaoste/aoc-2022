5b78eacd52a4bd04eecc091419a9bbe89db58f72
Thomas Kellermeier <dev@thomaskellermeier.de>
2022-12-04 15:22:21 +0100

    feat: submit sovled 3a

---
+++ day-3/README.md
@@ -0,0 +1,11 @@
+# Ruby
+
+## Setup
+
+Usually installed already on MacOS.
+
+## Run
+
+```
+  ruby ./exerciseA.rb
+```
---
+++ day-3/exerciseA.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+require "set"
+
+input_name = 'input.txt'
+sum = 0
+line = ''
+first_compartment = ''
+second_compartment = ''
+intersection = 0
+priority = 0
+
+File.foreach(input_name) do |input_line|
+  line = input_line.strip
+  line_length = line.length
+  first_compartment = line[0..line.length/2-1]
+  second_compartment = line[line.length/2..]
+  intersection = Set.new(first_compartment.chars) & Set.new(second_compartment.chars)
+  if (intersection.to_a.length != 1) or (first_compartment.length != second_compartment.length)
+    puts 'ERROR', line, first_compartment, second_compartment, intersection
+    exit
+  end
+  char_ord = intersection.to_a[0].ord
+  base_ord = char_ord < 'a'.ord ? 'A'.ord - 27 : 'a'.ord - 1
+  priority = char_ord - base_ord
+  sum += priority
+end
+
+puts 'Sum of priorities:', sum
