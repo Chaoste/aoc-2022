eb0fb463df254ece786f34b48949eb8e43e5875c
Thomas Kellermeier <dev@thomaskellermeier.de>
2022-12-11 13:21:53 +0100

    feat: submit sovled 10b

--- day-10/exercise-b.go
+++ day-10/exercise-b.go
@@ -17,19 +17,26 @@ func main() {
 
 		register := 1
 		cycle := 0
-		measurements := 0
 
 		increaseCycle:= func() {
-			cycle = cycle + 1
-			if ((cycle + 20) % 40 == 0) {
-				signal_strength := cycle * register
-				measurements = measurements + signal_strength
-				fmt.Printf("cycle: %d, register: %d, signal strength: %d\n", cycle, register, signal_strength)
+			pixel_pos := cycle % 40
+			if (cycle != 0 && pixel_pos == 0) {
+				print("\n")
 			}
+			if (pixel_pos >= register - 1 && pixel_pos <= register + 1) {
+				print("#")
+			} else {
+				print(".")
+			}
+
+			cycle = cycle + 1
 		}
 
     for scanner.Scan() {
 				line := scanner.Text()
+				// if(cycle > 210 && cycle < 225) {
+				// 	fmt.Printf("c: %d, r: %d, line: %s\n", cycle, register, line)
+				// }
 				if (line == "noop") {
 					increaseCycle()
 				} else if (line[:5] == "addx ") {
@@ -40,7 +47,7 @@ func main() {
 				}
     }
 
-		fmt.Printf("Final | cycle: %d, register: %d, signal strengths: %d\n", cycle, register, measurements)
+		fmt.Printf("\nFinal | cycle: %d, register: %d\n", cycle, register)
 	
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
