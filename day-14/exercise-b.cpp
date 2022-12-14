ab25081ff0df280ccd6b352f819f2a4dfa4211c4
Thomas Kellermeier <dev@thomaskellermeier.de>
2022-12-14 17:42:52 +0100

    feat: submit solved 14b

--- day-14/exercise-b.cpp
+++ day-14/exercise-b.cpp
@@ -22,22 +22,24 @@ vector<string> split (string s, string delimiter) {
     return res;
 }
 
-void print_map(string map[200][200]) {
+void print_map(string map[200][400]) {
   for(int y = 0; y < 200; y++) {
-    for(int x = 0; x < 200; x++) {
+    for(int x = 0; x < 400; x++) {
       cout << map[y][x];
     }
     cout << endl;
   }
 }
 
-void fill_map(string map[200][200]) {
+void fill_map(string map[200][400]) {
   string line;
   for(int y = 0; y < 200; y++){
-      for(int x = 0; x < 200; x++){
+      for(int x = 0; x < 400; x++){
           map[y][x] = ".";
       }
   }
+
+  int y_max = 0;
 
   ifstream input_file ("input.txt");
   if (input_file.is_open()) {
@@ -52,10 +54,12 @@ void fill_map(string map[200][200]) {
         vector<string> coords1 = split(pos1, ",");
         vector<string> coords2 = split(pos2, ",");
 
-        int x1 = stoi(coords1[0]) - 400;
+        int x1 = stoi(coords1[0]) - 300;
         int y1 = stoi(coords1[1]);
-        int x2 = stoi(coords2[0]) - 400;
+        int x2 = stoi(coords2[0]) - 300;
         int y2 = stoi(coords2[1]);
+
+        y_max = max(y_max, max(y1, y2));
 
         for( unsigned int x = min(x1, x2); x <= max(x1, x2); x = x + 1 ) {
           for( unsigned int y = min(y1, y2); y <= max(y1, y2); y = y + 1 ) {
@@ -64,16 +68,23 @@ void fill_map(string map[200][200]) {
         }
       }
     }
+
+    cout << "Floor level is at " << y_max << endl;
+
+    for( unsigned int x = 0; x < 400; x = x + 1 ) {
+      map[y_max+2][x] = '#';
+    }
+
     input_file.close();
   }
-  else cout << "Unable to open file";
+  else cout << "Unable to open file" << endl;
 }
 
-bool drop_sand(string map[200][200]) {
-  int sand[2] = {100, 0}; // 100|0 = 500|0
+bool drop_sand(string map[200][400]) {
+  int sand[2] = {200, 0}; // x=200|y=0 means 500|0
   while (map[sand[1]][sand[0]] == ".") {
-    if (sand[1]+1 > 199) {
-      // Sand drop fell into the void
+    if (sand[0] < 0 || sand[0] > 399) {
+      cout << "ERROR: Sand overflow" << endl;
       return false;
     } else if (map[sand[1]+1][sand[0]] == ".") {
       sand[1] = sand[1] + 1;
@@ -83,6 +94,10 @@ bool drop_sand(string map[200][200]) {
     } else if (map[sand[1]+1][sand[0]+1] == ".") {
       sand[1] = sand[1] + 1;
       sand[0] = sand[0] + 1;
+    } else if (sand[1] == 0) {
+      map[sand[1]][sand[0]] = 'o';
+      cout << "Reached the ceiling at " << sand[0] << "|" << sand[1] << endl;
+      return false;
     } else {
       map[sand[1]][sand[0]] = 'o';
     }
@@ -91,13 +106,15 @@ bool drop_sand(string map[200][200]) {
 }
 
 int main (int argc, char **_argv) {
-  // Coords: 0|0 = 400|0 ; 199|199 = 599|199
-  string map[200][200];
+  // Coords: x=0|y=0 means 300|0 ; 399|199 = 699|199
+  string map[200][400];
   fill_map(map);
   int i = 0;
   while (drop_sand(map)) {
     i = i + 1;
   }
+  // Increment counter to account for last step
+  i = i + 1;
   if (argc > 1) {
     // Accept any additional parameter to print
     print_map(map);
