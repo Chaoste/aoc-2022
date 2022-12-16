a1711bc68f6d7b2257c813fcd244a7be9bdcd005
Thomas Kellermeier <dev@thomaskellermeier.de>
2022-12-16 01:12:25 +0100

    feat: try out new range based approach

--- day-15/exercise-a.c
+++ day-15/exercise-a.c
@@ -5,7 +5,7 @@
 #include <regex.h>
 
 #define CHARS_PER_LINE 256
-#define NUMBER_OF_LINES 32
+#define NUMBER_OF_LINES 14
 #define MAP_SIZE 60
 
 unsigned int read_file(char lines[NUMBER_OF_LINES][CHARS_PER_LINE]) {
@@ -53,10 +53,10 @@ void parse_file(char lines[NUMBER_OF_LINES][CHARS_PER_LINE], int signals[NUMBER_
     /* Execute regular expression */
     reti = regexec(&regex, lines[i], maxGroups, groupArray, 0);
     if (!reti) {
-      signals[i][0] = extract_group_number(groupArray, 1, lines[i]) + MAP_SIZE / 2;
-      signals[i][1] = extract_group_number(groupArray, 2, lines[i]) + MAP_SIZE / 2;
-      beacons[i][0] = extract_group_number(groupArray, 3, lines[i]) + MAP_SIZE / 2;
-      beacons[i][1] = extract_group_number(groupArray, 4, lines[i]) + MAP_SIZE / 2;
+      signals[i][0] = extract_group_number(groupArray, 1, lines[i]);
+      signals[i][1] = extract_group_number(groupArray, 2, lines[i]);
+      beacons[i][0] = extract_group_number(groupArray, 3, lines[i]);
+      beacons[i][1] = extract_group_number(groupArray, 4, lines[i]);
     } else if (reti == REG_NOMATCH) {
       puts("No match");
       puts(lines[i]);
@@ -72,42 +72,43 @@ void parse_file(char lines[NUMBER_OF_LINES][CHARS_PER_LINE], int signals[NUMBER_
   regfree(&regex);
 }
 
-void fill_map(char map[MAP_SIZE][MAP_SIZE]) {
-  for(int i = 0; i < MAP_SIZE; ++i) {
-    for(int j = 0; j < MAP_SIZE; ++j) {
-      map[i][j] = '.';
-    }
+// Src: https://www.codesansar.com/c-programming-examples/sort-array-ascending-order-user-defined-function.htm
+void sort_by_start(int ranges[NUMBER_OF_LINES][2], int n) {
+ int i, j, temp0, temp1;
+ for(i = 0 ; i < n-1; i++) {
+  for(j = i+1; j < n; j++) {
+   if(ranges[i][0] > ranges[j][0]) {
+    temp0 = ranges[i][0];
+    temp1 = ranges[i][1];
+    ranges[i][0] = ranges[j][0];
+    ranges[i][1] = ranges[j][1];
+    ranges[j][0] = temp0;
+    ranges[j][1] = temp1;
+   }
   }
+ }
 }
 
-void print_map(char map[MAP_SIZE][MAP_SIZE]) {
-  for(int i = 0; i < MAP_SIZE; ++i) {
-    int row = (i - MAP_SIZE/2) % 100;
-    if (row < -9) {
-      printf("%d ", row);
-    } else if (row >=0 && row < 10) {
-      printf("  %d ", row);
-    } else {
-      printf(" %d ", row);
+int contains_number(int array[NUMBER_OF_LINES*2], int n, int value) {
+  int isElementPresent = 0;
+  for (int i = 0; i < n; i++) {
+    if (array[i] == value) {
+      isElementPresent = 1;
+      break;
     }
-    for(int j = 0; j < MAP_SIZE; ++j) {
-      printf("%c", map[j][i]);
-    }
-    printf("\n");
   }
-  printf("\n");
+  return isElementPresent;
 }
 
 int main(void) {
   char lines[NUMBER_OF_LINES][CHARS_PER_LINE];
   int signals[NUMBER_OF_LINES][2];
   int beacons[NUMBER_OF_LINES][2];
-  // static char map[MAP_SIZE][MAP_SIZE];
-  // puts("Fill map");
-  // fill_map(map);
-  int ranges[NUMBER_OF_LINES*2][2];
+  int ranges[NUMBER_OF_LINES][2];
   int range_count = 0;
   int target_row = 10;
+  int overlapping_items[NUMBER_OF_LINES*2]; // beacon or signal
+  int overlapping_items_count = 0;
 
   puts("Read file");
   unsigned int n_lines = read_file(lines);
@@ -116,37 +117,60 @@ int main(void) {
   parse_file(lines, signals, beacons);
 
   puts("Process signals");
-  for(int i = 0; i < 1; ++i) { // NUMBER_OF_LINES
+  for(int i = 0; i < NUMBER_OF_LINES; ++i) {
     int* signal = signals[i];
     int* beacon = beacons[i];
-    printf("%d %d %d \n", i, signal[0], signal[1]);
-    printf("%d %c %c \n", i, map[signal[0]][signal[1]], map[beacon[0]][beacon[1]]);
-    puts("add signal and beacon");
-    // map[signal[0]][signal[1]] = 'S';
-    // map[beacon[0]][beacon[1]] = 'B';
+    if (beacon[1] == target_row && !contains_number(overlapping_items, overlapping_items_count, beacon[0])) {
+      overlapping_items[overlapping_items_count++] = beacon[0];
+    }
+    if (signal[1] == target_row && !contains_number(overlapping_items, overlapping_items_count, signal[0])) {
+      overlapping_items[overlapping_items_count++] = signal[0];
+    }
     int diff = abs(signal[0]-beacon[0]) + abs(signal[1]-beacon[1]);
-    printf("Diff is %d\n", diff);
     for(int k = -diff; k <= diff; ++k) {
-      if (k == 20) {
-
-      }
-      for(int j = -diff; j <= diff; ++j) {
-        if (abs(j)+abs(k) <= diff && map[signal[0]+j][signal[1]+k] == '.') {
-          map[signal[0]+j][signal[1]+k] = '#';
-        }
+      if (signal[1]+k == target_row) {
+        int offset = diff - abs(k);
+        int range_start = signal[0] - offset;
+        int range_end = signal[0] + offset + 1;
+        printf("start=%d end=%d\n", range_start, range_end);
+        ranges[range_count][0] = range_start;
+        ranges[range_count][1] = range_end;
+        range_count++;
       }
     }
   }
 
-  // print_map(map);
-
-  int count_pos = 0;
-  for(int i = 0; i < MAP_SIZE; ++i) {
-    if (map[i][10+MAP_SIZE/2] == '#') {
-      ++count_pos;
+  // Idea from https://www.geeksforgeeks.org/merging-intervals/
+  sort_by_start(ranges, range_count);
+  int final_ranges[NUMBER_OF_LINES*2][2];
+  int final_range_count = 1;
+  // Add the first range (with the lowest start index)
+  final_ranges[0][0] = ranges[0][0];
+  final_ranges[0][1] = ranges[0][1];
+  for(int i = 1; i < range_count; ++i) {
+    int* range = ranges[i];
+    printf("Range: %d\n", range[0]);
+    int* previous_range = final_ranges[final_range_count-1];
+    if (previous_range[1] <= range[0]) {
+      // Not overlapping, add the new range
+      final_ranges[final_range_count][0] = range[0];
+      final_ranges[final_range_count][1] = range[1];
+      final_range_count++;
+    } else {
+      // Extend the existing range if the new end is greater
+      if (range[1] > final_ranges[final_range_count-1][1]) {
+        final_ranges[final_range_count-1][1] = range[1];
+      }
     }
   }
   
-  printf("Solution: %d\n", count_pos);
+  int blocked_fields = 0;
+  for(int i = 0; i < final_range_count; ++i) {
+    int* range = final_ranges[i];
+    int range_size = range[1] - range[0];
+    blocked_fields = blocked_fields + range_size;
+  }
+
+  printf("Solution: %d\n", blocked_fields - overlapping_items_count);
   return 0;
 }
